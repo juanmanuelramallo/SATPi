63227e1d52e3fff8375a9cf7c77b745db56211df
Juan Manuel Ramallo <ramallojuanm@gmail.com>
2017-09-15 06:00:31 -0300

    [MAIL & LOCALES] Updating locales

--- app/dashboards/alert_dashboard.rb
+++ app/dashboards/alert_dashboard.rb
@@ -21,8 +21,8 @@ class AlertDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :sensor,
     :id,
+    :sensor,
     :status,
     :created_at,
   ].freeze
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Satpi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Buenos Aires"
+    config.i18n.default_locale = :es
   end
 end
